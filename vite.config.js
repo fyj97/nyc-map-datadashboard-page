325dbc4465543925bcbb00fcde04315d417dd589
de2425c <154690407+de2425c@users.noreply.github.com>
2024-06-19 12:15:49 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nyc-map-datadashboard-page/"
+  base: "/nyc-map-datadashboard-page-testing/"
 })
